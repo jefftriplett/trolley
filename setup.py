cf1f24c0a8d587b1c5ff8532587ae785365cfa28
Jeff Triplett <jeff.triplett@gmail.com>
2015-04-19 18:07:30 -0500

    Added a simple setup.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Trolley',
+    version='0.0.1',
+    description='',
+    author='Jeff Triplett',
+    author_email='jeff.triplett@gmail.com',
+    url='http://github.com/jefftriplett/trolley',
+    packages=find_packages(),
+    py_modules=['trolley'],
+    entry_points={
+        'console_scripts': [
+            'trolley=trolley:cli',
+        ]
+    },
+    install_requires=[
+        'click',
+        'click-config',
+        'github3.py',
+        'trello',
+    ],
+)
