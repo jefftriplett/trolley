ebb9373edbddb1363c6b4f5a5ae6179bf4165379
Jeff Triplett <jeff.triplett@gmail.com>
2015-04-19 21:33:27 -0500

    Added pypi upload script back.

--- tasks.py
+++ tasks.py
@@ -13,9 +13,8 @@ def install():
     run('python setup.py build')
     run('python setup.py install')
 
-'''
+
 @task
 def pypi_upload():
     run('python setup.py sdist upload')
     run('python setup.py bdist_wheel upload')
-'''
