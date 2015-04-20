84f53e0ab49f95ba7fe52a7ffa92d534f199b71a
Jeff Triplett <jeff.triplett@gmail.com>
2015-04-19 19:50:48 -0500

    Added build system

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import run, task
+
+
+@task
+def build():
+    run('python setup.py build')
+    run('python setup.py sdist')
+    run('python setup.py bdist_wheel')
+
+
+@task
+def install():
+    run('python setup.py build')
+    run('python setup.py install')
+
+'''
+@task
+def pypi_upload():
+    run('python setup.py sdist upload')
+    run('python setup.py bdist_wheel upload')
+'''
