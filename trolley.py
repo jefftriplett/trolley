6b682d4089de64553cd3f798f02df3c24e2ccd8c
Jeff Triplett <jeff.triplett@gmail.com>
2015-04-19 22:55:22 -0500

    Bump version: 0.1.4 → 0.1.5

--- trolley.py
+++ trolley.py
@@ -17,7 +17,7 @@ from trello import TrelloApi
 __author__ = 'Jeff Triplett'
 __copyright__ = 'Copyright 2015, Jeff Triplett'
 __license__ = 'BSD'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 
 # hold auth state
